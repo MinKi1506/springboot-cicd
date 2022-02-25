cf286973abddb05c38d38d49a5c44da4021826fe
MinKi1506 <lunaris4537@gmail.com>
2022-02-25 10:11:13 +0900

    FIST commit

---
+++ src/main/java/cicd/springboot/cicd/springboot/ServletInitializer.java
@@ -0,0 +1,13 @@
+package cicd.springboot.cicd.springboot;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(CicdSpringbootApplication.class);
+	}
+
+}
---
+++ src/test/java/cicd/springboot/cicd/springboot/CicdSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package cicd.springboot.cicd.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
